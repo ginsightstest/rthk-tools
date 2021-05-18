35828800b7048947e5984203decef238f8372305
ginsightstest <karl-chan@bitbucket.org>
2021-05-18 22:26:43 +0100

    Fix EpisodesCsvReader parsing of NaNs / List

--- csv_reader_writer/episodes_csv_reader.py
+++ csv_reader_writer/episodes_csv_reader.py
@@ -1,5 +1,6 @@
 import ast
 import logging
+import math
 from typing import List
 
 import numpy as np
@@ -12,15 +13,20 @@ from util.dates import ymd_to_date
 class EpisodesCsvReader:
     def read_to_episodes(self, path: str) -> List[Episode]:
         def _nan_to_none(v: any) -> any:
-            return v if not pd.isnull(v) else None
+            if isinstance(v, float) and math.isnan(v):
+                return None
+            return v
+
+        def _parse_list(s: str) -> List[str]:
+            return ast.literal_eval(s) if s else []
 
         episodes = []
         frame = pd.read_csv(path,
                             parse_dates=['episode_date'],
                             date_parser=np.vectorize(ymd_to_date),
                             converters={
-                                'cids': ast.literal_eval,
-                                'category_names': ast.literal_eval
+                                'cids': _parse_list,
+                                'category_names': _parse_list
                             })
         logging.info(f"Read CSV file from: {path}")
         for i, row in frame.iterrows():
